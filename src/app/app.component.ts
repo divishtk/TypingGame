b63c65610367733e3cf001853561fae9a7eb56bc
divishtk <divisht.avion@gmail.com>
2021-12-24 19:55:14 +0530

    Pushed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {lorem,image} from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TypingGame';
+  randomText=lorem.sentence();
+  // randomImage=image.food();
+  inputText='';
+  flag=false;
+
+
+  onTypeEvent(event:any){
+
+    const inputWords=event.target.value;
+      this.inputText=inputWords;
+
+    if(this.inputText===this.randomText){
+      this.flag=!this.flag;   //flag will be true
+    }
+    else{
+      this.flag=false;
+    }
+    console.log(inputWords);
+
+
+
+  }
+
+  onCompareWords(randomWord:string,inputWord:string){
+
+    if(!inputWord){
+      return 'pending'
+    }
+    
+
+      return randomWord===inputWord ? 'CorrectWord':'Incorrect';
+
+  }
+  
+
 }
